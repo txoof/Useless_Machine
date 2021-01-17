a53455caa09553ecc2558a33f7e9c736be0ed4e6
txoof <aaron.ciuffo@gmail.com>
2021-01-17 14:45:19 +0100

    atom auto copy

--- ItsyBitsy Hardware/code.py
+++ ItsyBitsy Hardware/code.py
@@ -43,11 +43,11 @@ def move_servo(rate=50, direction=1, angle=None):
 
 
 while True:
-    print(servo_position)
-    if servo_position <= 0:
-        my_direction = 1
-    if servo_position >= 180:
-        my_direction = -1
+    # print(servo_position)
+    # if servo_position <= 0:
+    #     my_direction = 1
+    # if servo_position >= 180:
+    #     my_direction = -1
 
 
 
