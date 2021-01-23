58e0d2ff769c5a199381066c197f0f6beb9db39c
txoof <aaron.ciuffo@gmail.com>
2021-01-23 19:54:58 +0100

    atom auto copy

--- ItsyBitsy Hardware/code.py
+++ ItsyBitsy Hardware/code.py
@@ -49,6 +49,6 @@ while True:
         print(f'limitsw statechange: {not  limitsw.value}')
         limitsw_last = limitsw.value
     servo.duty_cycle = servo_duty_cycle(1.0)
-    time.sleep(1.0)
+    # time.sleep(1.0)
     servo.duty_cycle = servo_duty_cycle(2.0)
-    time.sleep(1.0)
+    # time.sleep(1.0)
