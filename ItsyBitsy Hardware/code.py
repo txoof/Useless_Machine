e1f23171b96f0bfa34f85d2e99ebe43ed9b91f0a
txoof <aaron.ciuffo@gmail.com>
2021-01-23 20:00:08 +0100

    atom auto copy

--- ItsyBitsy Hardware/code.py
+++ ItsyBitsy Hardware/code.py
@@ -50,5 +50,5 @@ while True:
         limitsw_last = limitsw.value
     servo.duty_cycle = servo_duty_cycle(.6)
     time.sleep(1.5)
-    servo.duty_cycle = servo_duty_cycle(2.6)
+    servo.duty_cycle = servo_duty_cycle(2.5)
     time.sleep(1.5)
