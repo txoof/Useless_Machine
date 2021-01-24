f8ffd8f5250935e38c45f88ff593d4ab604455e6
txoof <aaron.ciuffo@gmail.com>
2021-01-24 13:55:28 +0100

    atom auto copy

--- ItsyBitsy Hardware/code.py
+++ ItsyBitsy Hardware/code.py
@@ -18,3 +18,56 @@ RESOLUTION_MAX = 0.04 # largest steps to take when moving
 
 SERVO_MIN = 0
 SERVO_MAX = 180
+
+# pin objects
+limit_switch_pin = digitalio.DigitalInOut(LIMIT_SWITCH_PHY)
+limit_switch_pin.direction = digitalio.Direction.INPUT
+limit_switch_pin.pull = digitalio.Pull.DOWN
+limit_switch = Debouncer(limit_switch_pin)
+
+direction_switch_pin = digitalio.DigitalInOut(DIRECTION_SWITCH_PHY)
+direction_switch_pin.direction = digitalio.Direction.INPUT
+direction_switch_pin.pull = digitalio.Pull.DOWN
+direction_switch = Debouncer(direction_switch_pin)
+
+servo = pulseio.PWMOut(SERVO_PWM_PHY, duty_cycle=2**15, frequency=50)
+
+def map_range(a, b, s):
+    '''Map range (min a, max a) to range (min b, max b) for value s
+    '''
+    (a1, a2), (b1, b2) = a, b
+    return b1 + ((s - a1) * (b2 - b1) / (a2 - a1))
+
+def angle_to_duty(angle, frequency=50):
+    '''convert angle between ANGLE_MIN-ANGLE_MAX to a position between DUTY_MIN-DUTY_MAX'''
+    pulse_ms = map_range((ANGLE_MIN, ANGLE_MAX), (DUTY_MIN, DUTY_MAX), angle)
+
+    period_ms = 1.0 / frequency * 1000.0
+    return int(pulse_ms / (period_ms / 65535.0))
+
+
+def rotate_to_angle(current_angle, dest_angle, speed):
+    '''rotate from current_angle to dest_angle at speed
+    Args:
+        current_angle (real): angle between SERVO_MIN and SERVO_MAX
+        dest_angle (real): angle between SERVO_MIN and SERVO_MAX
+        speed (real): between 0 and 1
+    '''
+
+    direction = 1 if current_angle < dest_angle else -1
+    endstop = limit_switch if direction == -1 else direction_switch_pin
+
+    endstop.update()
+
+    step_size = map_range((0, 1), (RESOLUTION_MIN, RESOLUTION_MAX), speed)
+    steps = int(abs(current_angle-dest_angle))
+
+    for i in range(0, steps):
+        if endstop.value:
+            break
+        current_angel = current_angle + step_size * direction_switch
+        servo.duty_cycle = angle_to_duty(current_angle)
+
+    return current_angle
+
+rotate_to_angle(90)
