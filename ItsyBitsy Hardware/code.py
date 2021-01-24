70fe4c3a07412e63f609e3ea532a45569711932c
txoof <aaron.ciuffo@gmail.com>
2021-01-24 14:34:00 +0100

    atom auto copy

--- ItsyBitsy Hardware/code.py
+++ ItsyBitsy Hardware/code.py
@@ -14,8 +14,8 @@ SERVO_PWM_PHY = board.D10
 DUTY_MIN = 0.5 # 0 degrees
 DUTY_MAX = 2.5 # 180 degrees
 
-RESOLUTION_MIN = 0.05 # smallest steps to take when moving
-RESOLUTION_MAX = 10 # largest steps to take when moving
+RESOLUTION_MIN = 0.03 # smallest angle steps to take when moving
+RESOLUTION_MAX = 10 # largest angle steps to take when moving
 
 ANGLE_MIN = 0
 ANGLE_MAX = 180
