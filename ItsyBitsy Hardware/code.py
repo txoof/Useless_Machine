d04fe68c5babee5aef6b2b35a0c6946744c7b217
txoof <aaron.ciuffo@gmail.com>
2021-01-24 13:21:43 +0100

    atom auto copy

--- ItsyBitsy Hardware/code.py
+++ ItsyBitsy Hardware/code.py
@@ -2,12 +2,19 @@ import board
 import digitalio
 import pulseio
 
-from hello import *
-
 from adafruit_debouncer import Debouncer
 
 # physical switches
 LIMIT_SWITCH_PHY = board.D9
-DIRECTION_SWITCH_PHY = board
+DIRECTION_SWITCH_PHY = board.D7
 
-print_hello()
+SERVO_PWM_PHY = board.D10
+
+DUTY_MIN = 0.5 # 0 degrees
+DUTY_MAX = 2.5 # 180 degrees
+
+RESOLUTION_MIN = 0.002 # smallest steps to take when moving
+RESOLUTION_MAX = 0.04 # largest steps to take when moving
+
+SERVO_MIN = 0
+SERVO_MAX = 180
