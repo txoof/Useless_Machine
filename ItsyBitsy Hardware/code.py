c61067dcbfcbdb2e40c81b28d6249da397f35ffa
txoof <aaron.ciuffo@gmail.com>
2021-01-24 16:27:41 +0100

    atom auto copy

--- ItsyBitsy Hardware/code.py
+++ ItsyBitsy Hardware/code.py
@@ -92,11 +92,12 @@ def rotate_to_angle(current_angle, dest_angle, speed):
             print('hit upper limit value -- breaking out')
             break_out = True
 
-        current_angle = current_angle + (step_size * direction)
-        servo.duty_cycle = angle_to_duty(current_angle)
+
         if break_out:
             print('breaking out')
             break
+        current_angle = current_angle + (step_size * direction)
+        servo.duty_cycle = angle_to_duty(current_angle)        
 
     return current_angle
 
