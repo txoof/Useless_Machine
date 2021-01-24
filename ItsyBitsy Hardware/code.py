7006371f5ccc42dbe35b5ff784fff51428080eff
txoof <aaron.ciuffo@gmail.com>
2021-01-24 16:35:23 +0100

    atom auto copy

--- ItsyBitsy Hardware/code.py
+++ ItsyBitsy Hardware/code.py
@@ -99,7 +99,7 @@ def rotate_to_angle(current_angle, dest_angle, speed):
             break
         current_angle = current_angle + (step_size * direction)
         servo.duty_cycle = angle_to_duty(current_angle)
-
+    print(f'returning: {current_angle}')
     return current_angle
 
 
