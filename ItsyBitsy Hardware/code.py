cdb79748d0d98ced707b8272adb51c339eae895a
txoof <aaron.ciuffo@gmail.com>
2021-01-25 21:27:47 +0100

    atom auto copy

--- ItsyBitsy Hardware/code.py
+++ ItsyBitsy Hardware/code.py
@@ -58,7 +58,7 @@ def go_to_angle(dest_angle):
     servo.duty_cycle = angle_to_duty(dest_angle)
 
 
-def rotate_to_angle(current_angle, dest_angle, speed):
+def rotate_to_angle(current_angle, dest_angle, speed=0.08):
     '''rotate from current_angle to dest_angle at speed
     Args:
         current_angle (real): angle between SERVO_MIN and SERVO_MAX
