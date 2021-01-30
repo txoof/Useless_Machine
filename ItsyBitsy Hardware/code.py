4cd1f4ee400b5ea7e38af44138bb8c07bb999738
txoof <aaron.ciuffo@gmail.com>
2021-01-30 12:03:26 +0100

    atom auto copy

--- ItsyBitsy Hardware/code.py
+++ ItsyBitsy Hardware/code.py
@@ -46,7 +46,70 @@ def go_to_angle(dest_angle):
     servo.duty_cycle = angle_to_duty(dest_angle)
 
 
-def rotate_to_angle(current_angle, dest_angle, speed=0.08):
+# def rotate_to_angle(current_angle, dest_angle, speed=0.08):
+#     '''rotate from current_angle to dest_angle at speed
+#     Args:
+#         current_angle (real): angle between SERVO_MIN and SERVO_MAX
+#         dest_angle (real): angle between SERVO_MIN and SERVO_MAX
+#         speed (real): between 0 and 1
+#     '''
+#     def check_angle(angle):
+#         if angle > HOME_HIGH:
+#             angle = HOME_HIGH
+#         if angle < HOME_LOW:
+#             angle = HOME_LOW
+#         return angle
+#
+#     direction = 1 if current_angle < dest_angle else -1
+#
+#     break_out = False
+#
+#     endstop_hit = None
+#
+#     step_size = map_range((0, 1), (RESOLUTION_MIN, RESOLUTION_MAX), speed)
+#     steps = int(abs((current_angle-dest_angle)/step_size))
+#
+#     print(f'\n\nROTATE to {dest_angle}; speed: {speed}')
+#
+#
+#     for i in range(0, steps+1):
+#         limit_switch.update()
+#         direction_switch.update()
+#
+#         # check for endstop collisions
+#         # limit switch
+#         if direction == -1 and limit_switch.value:
+#             break_out = True
+#             endstop_hit = 'limit_switch'
+#
+#
+#         if direction == 1 and direction_switch.value:
+#             break_out = True
+#             endstop_hit = 'direction_switch changed while attacking'
+#
+#
+#
+#         if direction == -1 and not direction_switch.value:
+#             break_out = True
+#             endstop_hit = 'direction_switch changed while retreating'
+#
+#         if break_out:
+#             print(f'{endstop_hit}')
+#             break
+#
+#         else:
+#             current_angle = current_angle + (step_size * direction)
+#
+#             # if over-run in positive or negative, set to max or min as appropriate
+#             current_angle = check_angle(current_angle)
+#
+#
+#             servo.duty_cycle = angle_to_duty(current_angle)
+#
+#     current_angle = check_angle(current_angle)
+#     return current_angle
+
+def rotate_to_angle(current_angle, dest_angle, attack, speed=0.08):
     '''rotate from current_angle to dest_angle at speed
     Args:
         current_angle (real): angle between SERVO_MIN and SERVO_MAX
@@ -60,6 +123,7 @@ def rotate_to_angle(current_angle, dest_angle, speed=0.08):
             angle = HOME_LOW
         return angle
 
+    # set the direction multiplier
     direction = 1 if current_angle < dest_angle else -1
 
     break_out = False
@@ -71,7 +135,6 @@ def rotate_to_angle(current_angle, dest_angle, speed=0.08):
 
     print(f'\n\nROTATE to {dest_angle}; speed: {speed}')
 
-    direction_changed = False
 
     for i in range(0, steps+1):
         limit_switch.update()
@@ -81,35 +144,41 @@ def rotate_to_angle(current_angle, dest_angle, speed=0.08):
         # limit switch
         if direction == -1 and limit_switch.value:
             break_out = True
-            endstop_hit = 'limit_switch'
-
+            endstop_hit = 'bottom endstop hit'
 
         if direction == 1 and direction_switch.value:
-            break_out = True
-            endstop_hit = 'direction_switch changed while attacking'
+            breakout = True
+            endstop_hit = 'top endstop hit'
 
 
 
-        if direction == -1 and not direction_switch.value:
-            break_out = True
-            endstop_hit = 'direction_switch changed while retreating'
-            direction_changed = True
-
+        #
+        # if direction == 1 and direction_switch.value:
+        #     break_out = True
+        #     endstop_hit = 'direction_switch changed while attacking'
+        #
+        #
+        #
+        # if direction == -1 and not direction_switch.value:
+        #     break_out = True
+        #     endstop_hit = 'direction_switch changed while retreating'
+        #
         if break_out:
             print(f'{endstop_hit}')
             break
-
-        else:
-            current_angle = current_angle + (step_size * direction)
-
-            # if over-run in positive or negative, set to max or min as appropriate
-            current_angle = check_angle(current_angle)
-
-
-            servo.duty_cycle = angle_to_duty(current_angle)
+        # 
+        # else:
+        #     current_angle = current_angle + (step_size * direction)
+        #
+        #     # if over-run in positive or negative, set to max or min as appropriate
+        #     current_angle = check_angle(current_angle)
+        #
+        #
+        #     servo.duty_cycle = angle_to_duty(current_angle)
 
     current_angle = check_angle(current_angle)
     return current_angle
+
 
 # pin objects
 limit_switch_pin = digitalio.DigitalInOut(LIMIT_SWITCH_PHY)
@@ -161,7 +230,7 @@ while True:
             print('**********run forwards**********')
             # current_angle = rotate_to_angle(current_angle, HOME_HIGH)
             for i in attack_program:
-                current_angle = rotate_to_angle(current_angle, i[0], i[1])
+                current_angle = rotate_to_angle(current_angle, i[0], attack=True, i[1])
 
     if not limit_switch.value and direction_switch.value:
         if current_angle <= HOME_LOW:
@@ -170,7 +239,7 @@ while True:
             print('**********run backwards**********')
             # current_angle = rotate_to_angle(current_angle, HOME_LOW)
             for i in retreat_program:
-                current_angle = rotate_to_angle(current_angle, i[0], i[1])
+                current_angle = rotate_to_angle(current_angle, i[0], attack=False, i[1])
 
     if  limit_switch.value != limit_last:
         limit_last = limit_switch.value
