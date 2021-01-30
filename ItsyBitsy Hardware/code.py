fcd9a230ce9f93b1718bdcecaac00632cc0f20b3
txoof <aaron.ciuffo@gmail.com>
2021-01-30 13:30:29 +0100

    atom auto copy

--- ItsyBitsy Hardware/code.py
+++ ItsyBitsy Hardware/code.py
@@ -46,69 +46,6 @@ def go_to_angle(dest_angle):
     servo.duty_cycle = angle_to_duty(dest_angle)
 
 
-# def rotate_to_angle(current_angle, dest_angle, speed=0.08):
-#     '''rotate from current_angle to dest_angle at speed
-#     Args:
-#         current_angle (real): angle between SERVO_MIN and SERVO_MAX
-#         dest_angle (real): angle between SERVO_MIN and SERVO_MAX
-#         speed (real): between 0 and 1
-#     '''
-#     def check_angle(angle):
-#         if angle > HOME_HIGH:
-#             angle = HOME_HIGH
-#         if angle < HOME_LOW:
-#             angle = HOME_LOW
-#         return angle
-#
-#     direction = 1 if current_angle < dest_angle else -1
-#
-#     break_out = False
-#
-#     endstop_hit = None
-#
-#     step_size = map_range((0, 1), (RESOLUTION_MIN, RESOLUTION_MAX), speed)
-#     steps = int(abs((current_angle-dest_angle)/step_size))
-#
-#     print(f'\n\nROTATE to {dest_angle}; speed: {speed}')
-#
-#
-#     for i in range(0, steps+1):
-#         limit_switch.update()
-#         direction_switch.update()
-#
-#         # check for endstop collisions
-#         # limit switch
-#         if direction == -1 and limit_switch.value:
-#             break_out = True
-#             endstop_hit = 'limit_switch'
-#
-#
-#         if direction == 1 and direction_switch.value:
-#             break_out = True
-#             endstop_hit = 'direction_switch changed while attacking'
-#
-#
-#
-#         if direction == -1 and not direction_switch.value:
-#             break_out = True
-#             endstop_hit = 'direction_switch changed while retreating'
-#
-#         if break_out:
-#             print(f'{endstop_hit}')
-#             break
-#
-#         else:
-#             current_angle = current_angle + (step_size * direction)
-#
-#             # if over-run in positive or negative, set to max or min as appropriate
-#             current_angle = check_angle(current_angle)
-#
-#
-#             servo.duty_cycle = angle_to_duty(current_angle)
-#
-#     current_angle = check_angle(current_angle)
-#     return current_angle
-
 def rotate_to_angle(current_angle, dest_angle, attack, speed=0.08):
     '''rotate from current_angle to dest_angle at speed
     Args:
@@ -191,6 +128,13 @@ direction_last = direction_switch.update()
 # servo.duty_cycle = angle_to_duty(HOME_LOW+10)
 go_to_angle(HOME_LOW-.5)
 # servo.duty_cycle = angle_to_duty(HOME_LOW-.5)
+
+l = [50, 65, 70, 75, 80, 90, 100]
+
+for in in l:
+    print(f'angle: {i}')
+    go_to_angle(i)'
+    time.sleep(1.5)
 
 current_angle = HOME_LOW
 
