90c04f851df7170b24078359e366ae35807ea33e
txoof <aaron.ciuffo@gmail.com>
2021-01-30 21:56:42 +0100

    atom auto copy

--- ItsyBitsy Hardware/code.py
+++ ItsyBitsy Hardware/code.py
@@ -234,7 +234,7 @@ while True:
             print('timeout elapsed -- shutting down battery power')
             relay_pin = True
         else:
-            print(f'timeout remaining: {time.monotonic - off_timer}')
+            print(f'timeout remaining: {time.monotonic() - off_timer}')
 
     if direction_switch.value == False:
         # reset current angle to max/min
