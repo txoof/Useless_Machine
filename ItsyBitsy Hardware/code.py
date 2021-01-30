7f22fdd009006042544f4763b91b09ecc825979f
txoof <aaron.ciuffo@gmail.com>
2021-01-30 15:50:02 +0100

    atom auto copy

--- ItsyBitsy Hardware/code.py
+++ ItsyBitsy Hardware/code.py
@@ -165,8 +165,8 @@ peek_a_boo = [(62, .3, None), (None, None, 1.5),
               (HOME_HIGH, .1, None)]
 
 # attack_program = [(100, 0.55, None), (125, 0.1, None), (150, 0.59, None), (HOME_HIGH, 0.1, None)]
-attack_program = peek_a_boo
-# attack_program = [(90, 0.99, None), (145, 0.1, None), (90, 0.3, None), (HOME_HIGH, 0.1, None)]
+# attack_program = peek_a_boo
+attack_program = [(90, 0.99, None), (145, 0.1, None), (90, 0.3, None), (HOME_HIGH, 0.1, None)]
 retreat_program = [(150, .1, None), (50, 0.7, None), (HOME_LOW, 0.01, None)]
 
 
