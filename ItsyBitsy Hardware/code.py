6a42dea8bcdbf83522c6fd67db8c9cffb5d499fb
txoof <aaron.ciuffo@gmail.com>
2021-01-30 12:19:55 +0100

    atom auto copy

--- ItsyBitsy Hardware/code.py
+++ ItsyBitsy Hardware/code.py
@@ -128,14 +128,12 @@ def rotate_to_angle(current_angle, dest_angle, attack, speed=0.08):
 
     break_out = False
 
-
     breakout_msg = None
 
     step_size = map_range((0, 1), (RESOLUTION_MIN, RESOLUTION_MAX), speed)
     steps = int(abs((current_angle-dest_angle)/step_size))
 
     print(f'ROTATE to {dest_angle}; speed: {speed}')
-
 
     for i in range(0, steps+1):
         limit_switch.update()
@@ -174,7 +172,7 @@ def rotate_to_angle(current_angle, dest_angle, attack, speed=0.08):
             servo.duty_cycle = angle_to_duty(current_angle)
 
     current_angle = check_angle(current_angle)
-    print('\n')
+    print('')
     return current_angle, break_out
 
 
