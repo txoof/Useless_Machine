bd82e95242361286b3bb564d0b52947d6897148c
txoof <aaron.ciuffo@gmail.com>
2021-01-30 14:09:02 +0100

    atom auto copy

--- ItsyBitsy Hardware/code.py
+++ ItsyBitsy Hardware/code.py
@@ -19,7 +19,7 @@ DUTY_MIN = 0.5 # 0 degrees
 DUTY_MAX = 2.5 # 180 degrees
 
 RESOLUTION_MIN = 0.04 # smallest angle steps to take when moving
-RESOLUTION_MAX = 10 # largest angle steps to take when moving
+RESOLUTION_MAX = 7 # largest angle steps to take when moving
 
 ANGLE_MIN = 0
 ANGLE_MAX = 180
