db155b7cb75e9370ed6c1ddd05452da47243519c
txoof <aaron.ciuffo@gmail.com>
2021-02-05 22:32:38 +0100

    atom auto copy

--- ItsyBitsy Hardware/code.py
+++ ItsyBitsy Hardware/code.py
@@ -85,6 +85,67 @@ def go_to_angle(dest_angle):
     print(f'moving to: {dest_angle}')
     servo.duty_cycle = angle_to_duty(dest_angle)
 
+def rotate_to_angle(current_angle, dest_angle, attack, speed=0.08):
+    '''rotate from current_angle to dest_angle at speed
+    Args:
+        current_angle (real): angle between SERVO_MIN and SERVO_MAX
+        dest_angle (real): angle between SERVO_MIN and SERVO_MAX
+        attack (bool): True - arm moves out (ccw), False - arm moves in (cw)
+        speed (real): between 0 and 1
+    '''
+    def check_angle(angle):
+        if angle > HOME_HIGH:
+            angle = HOME_HIGH
+        if angle < HOME_LOW:
+            angle = HOME_LOW
+        return angle
+
+    # set the direction multiplier
+    direction = 1 if current_angle < dest_angle else -1
+
+    break_out = False
+
+    breakout_msg = None
+
+    step_size = map_range((0, 1), (RESOLUTION_MIN, RESOLUTION_MAX), speed)
+    steps = int(abs((current_angle-dest_angle)/step_size))
+
+    print(f'ROTATE to {dest_angle}; speed: {speed}')
+
+    for i in range(0, steps+1):
+        limit_switch.update()
+        direction_switch.update()
+        # print(f'direction: {direction}\nattack: {attack}\ndirection_switch: {direction_switch.value}\n')
+
+        if direction == 1 and attack == True and direction_switch.value == True:
+            break_out = True
+            breakout_msg = 'direction switch changed to "True" '
+
+        if direction == -1 and attack == False and direction_switch.value == False:
+            break_out = True
+            breakout_msg = 'direction switch changed to "False"'
+
+        if direction == -1 and limit_switch.value == True:
+            break_out = True
+            breakout_msg = 'bottom limit siwtch hit'
+
+
+        if break_out:
+            print(f'{breakout_msg}')
+            break
+
+        else:
+            current_angle = current_angle + (step_size * direction)
+
+            # if over-run in positive or negative, set to max or min as appropriate
+            current_angle = check_angle(current_angle)
+            servo.duty_cycle = angle_to_duty(current_angle)
+
+    current_angle = check_angle(current_angle)
+    print('')
+    return current_angle, break_out
+
+
 
 ##### GLOBALS  #####
 # last state of limit switch
