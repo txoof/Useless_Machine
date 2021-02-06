025d5d63758353fbc778ed5853ce180155c9bfb0
txoof <aaron.ciuffo@gmail.com>
2021-02-06 13:03:48 +0100

    atom auto copy

--- ItsyBitsy Hardware/code.py
+++ ItsyBitsy Hardware/code.py
@@ -237,7 +237,7 @@ ret_standard = [(150, .6, None),
 ret_aggressive = [(150, .9, None),
                (70, .9, None),
                (50, .8, None),
-               (HOME_LOW, 0.5)]
+               (HOME_LOW, 0.5, None)]
 
 
 
