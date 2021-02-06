2dd8887e3571f5b1feb31b1f2ff5f54696655cb2
txoof <aaron.ciuffo@gmail.com>
2021-02-06 14:11:38 +0100

    atom auto copy

--- ItsyBitsy Hardware/code.py
+++ ItsyBitsy Hardware/code.py
@@ -66,7 +66,7 @@ while True:
     # color_chase(BLUE, 0.05)
     # color_chase(PURPLE, 0.05)
 
-    # rainbow_cycle(.03)  # Increase the number to slow down the rainbow
+    rainbow_cycle(.03)  # Increase the number to slow down the rainbow
 # import board
 # import digitalio
 # import time
