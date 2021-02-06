360a424f506b09a167fe36a007ce24fb17d1b91a
txoof <aaron.ciuffo@gmail.com>
2021-02-06 14:07:27 +0100

    atom auto copy

--- ItsyBitsy Hardware/code.py
+++ ItsyBitsy Hardware/code.py
@@ -48,25 +48,25 @@ BLUE = (0, 0, 255)
 PURPLE = (180, 0, 255)
 
 while True:
-    pixels.fill(RED)
-    pixels.show()
-    # Increase or decrease to change the speed of the solid color change.
-    time.sleep(1)
-    pixels.fill(GREEN)
-    pixels.show()
-    time.sleep(1)
-    pixels.fill(BLUE)
-    pixels.show()
-    time.sleep(1)
+    # pixels.fill(RED)
+    # pixels.show()
+    # # Increase or decrease to change the speed of the solid color change.
+    # time.sleep(1)
+    # pixels.fill(GREEN)
+    # pixels.show()
+    # time.sleep(1)
+    # pixels.fill(BLUE)
+    # pixels.show()
+    # time.sleep(1)
+    #
+    # color_chase(RED, 0.1)  # Increase the number to slow down the color chase
+    # color_chase(YELLOW, 0.1)
+    # color_chase(GREEN, 0.1)
+    # color_chase(CYAN, 0.1)
+    # color_chase(BLUE, 0.1)
+    # color_chase(PURPLE, 0.1)
 
-    color_chase(RED, 0.1)  # Increase the number to slow down the color chase
-    color_chase(YELLOW, 0.1)
-    color_chase(GREEN, 0.1)
-    color_chase(CYAN, 0.1)
-    color_chase(BLUE, 0.1)
-    color_chase(PURPLE, 0.1)
-
-    rainbow_cycle(0)  # Increase the number to slow down the rainbow
+    rainbow_cycle(3)  # Increase the number to slow down the rainbow
 # import board
 # import digitalio
 # import time
