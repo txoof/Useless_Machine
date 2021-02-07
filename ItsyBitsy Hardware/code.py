5b94e977ca3732e21d5c4cf3adefeb74a719aaf3
txoof <aaron.ciuffo@gmail.com>
2021-02-07 13:42:23 +0100

    atom auto copy

--- ItsyBitsy Hardware/code.py
+++ ItsyBitsy Hardware/code.py
@@ -303,10 +303,11 @@ ret_standard = [(150, .6, None, BLUE),
                 (None, None, 0.25)]
 
 ret_just_checking = [(150, .5, None, BLUE),
-                     (None, None, .5, NAVY),
+                     (None, None, 1, NAVY),
                      (120, .5, None, BLUE),
-                     (None, None, .5, NAVY),
+                     (None, None, 1, NAVY),
                      (150, .5, None, RED),
+                     (None, None, 1, NAVY),
                      (50, .8, None, BLUE),
                      (None, None, .5, NAVY),
                      (62, .3, None, RED),
@@ -314,6 +315,7 @@ ret_just_checking = [(150, .5, None, BLUE),
                      (50, .1, None, BLUE),
                      (None, None, .5, NAVY),
                      (62, .3, None, RED),
+                     (None, None, .5)
                      (50, .1, None, BLUE),
                      (HOME_LOW, 0.05, None, BLUE)]
 
