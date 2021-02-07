ddceafe64176c07a18616cc80e8f8c2bfc9bc8e2
txoof <aaron.ciuffo@gmail.com>
2021-02-07 12:47:12 +0100

    atom auto copy

--- ItsyBitsy Hardware/code.py
+++ ItsyBitsy Hardware/code.py
@@ -311,7 +311,7 @@ while True:
         if not is_shutdown:
             print(f'time to shutdown: {time.monotonic() - shutdown_timer - SHUTDOWN_TIMEOUT}')
         print(f'CPU Temperature: {cpu.temperature}')
-        print(f'Random Number: {urandom}')
+        print(f'Random Number: {urandom()}')
     limit_switch.update()
     direction_switch.update()
 
